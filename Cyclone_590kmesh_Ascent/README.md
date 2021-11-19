6f8139b13af14e5debe913e1b138ae89a9f55604
Chonglin Zhang <zhangchonglin@gmail.com>
2021-11-19 15:11:29 -0500

    add Ascent case run on 1 node with 6 GPUs

---
+++ Cyclone_590kmesh_Ascent/README.md
@@ -0,0 +1,12 @@
+On Ascent, compile with:
+ - kokkos 3.1.01
+ - gcc 7.4.0
+ - cuda 10.2.89
+ - cmake 3.18.2
+ - omega_h and pumi_pic master branch
+ - PETSc 3.15.2
+ - XGCm master branch
+
+Case run on 1 node of OLCF Ascent system:
+ - 6 GPUs used
+ - 20 million particles per GPU
---
+++ Cyclone_590kmesh_Ascent/xgcm_cyclone_ascent_submission.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BSUB -P GEN170
+#BSUB -W 0:05
+#BSUB -nnodes 1
+#BSUB -J Cyclone-590k
+#BSUB -o Cyclone-590k.%J
+#BSUB -e Cyclone-590k.%J
+
+module load cuda/10.2.89
+
+date
+jsrun -n 6 -a 1 -c 1 -g 1 --smpiargs "-gpu" \
+./XGCm --kokkos-threads=1 590kmesh.osh 590kmesh_6.cpn \
+1 1 bfs bfs 0 0 0 3 input_xgcm petsc petsc_xgcm.rc \
+-use_gpu_aware_mpi 0
+
+date
