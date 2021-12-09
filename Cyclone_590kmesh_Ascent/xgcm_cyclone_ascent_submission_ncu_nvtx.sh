8acbcf75142215f2beb4f4c5f41a14bfd5550ff2
Chonglin Zhang <zhangchonglin@gmail.com>
2021-12-09 17:10:50 -0500

    update nsight compute run script

--- Cyclone_590kmesh_Ascent/xgcm_cyclone_ascent_submission_ncu_nvtx.sh
+++ Cyclone_590kmesh_Ascent/xgcm_cyclone_ascent_submission_ncu_nvtx.sh
@@ -14,7 +14,7 @@ export KOKKOS_PROFILE_LIBRARY=/ccsopen/home/zhangc20/xgcm/kokkos-tools/kp_nvprof
 date
 jsrun -n 6 -a 1 -c 1 -g 1 --smpiargs "-gpu" \
 ncu --target-processes all -f -o XGCm_profile_ncu_nvtx \
---nvtx --nvtx-include "gyroScatterEFF/" \
+--nvtx --nvtx-include "gyroScatterEFF/" --set full \
 ./XGCm --kokkos-threads=1 590kmesh.osh 590kmesh_6.cpn \
 1 1 bfs bfs 0 0 0 3 input_xgcm_1step petsc petsc_xgcm.rc \
 -use_gpu_aware_mpi 0
