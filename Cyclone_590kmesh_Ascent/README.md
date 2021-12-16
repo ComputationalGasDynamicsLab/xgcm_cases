a1a4f3e7a9e934461aeb548fd2095554f3cdf7f8
Chonglin Zhang <zhangchonglin@gmail.com>
2021-12-16 16:00:29 -0500

    update Ascent run script with nsight compute

--- Cyclone_590kmesh_Ascent/README.md
+++ Cyclone_590kmesh_Ascent/README.md
@@ -11,4 +11,20 @@ Case run on 1 node of OLCF Ascent system:
  - 6 GPUs used
  - 20 million particles per GPU
 
-To use `kokkos-tools/kp_nvprof_connector`, need to build with `develop` branch of `kokkos-tools`.
+Note:
+- To get correct kernel names, need to use `kokkos-tools/kp_nvprof_connector`;
+- Build `kp_nvprof_connector` using the `develop` branch of `kokkos-tools`.
+- To use `nsight compute` to profile a specific GPU kernel, needs
+  to add the following in the run script within `srun` line:
+```
+ncu --target-processes all -f -o XGCm_profile_ncu_nvtx \
+--nvtx --nvtx-include "gyroScatterEFF_cab/" --set full \
+``
+- Additionally, to profile multiple GPU kernels at the same time, need to do:
+```
+--nvtx_include "kernel_A/*/kernel_B/*/kernel_c/"
+```
+- Below is an example:
+```
+--nvtx --nvtx-include "gyroScatterEFF/*/ion_push_op/" --set full
+```
--- Cyclone_590kmesh_Ascent/xgcm_cyclone_ascent_submission_ncu_nvtx.sh
+++ Cyclone_590kmesh_Ascent/xgcm_cyclone_ascent_submission_ncu_nvtx.sh
@@ -14,12 +14,18 @@ export KOKKOS_PROFILE_LIBRARY=/ccsopen/home/zhangc20/xgcm/kokkos-tools/kp_nvprof
 date
 jsrun -n 6 -a 1 -c 1 -g 1 --smpiargs "-gpu" \
 ncu --target-processes all -f -o XGCm_profile_ncu_nvtx \
---nvtx --nvtx-include "savePtclPos/" --set full \
+--nvtx --nvtx-include "gyroScatterEFF/" --set full \
 ./XGCm --kokkos-threads=1 590kmesh.osh 590kmesh_6.cpn \
 1 1 bfs bfs 0 0 0 3 input_xgcm_1step petsc petsc_xgcm.rc \
 -use_gpu_aware_mpi 0
 
 date
+
+# Belows are used to profile other GPU kernels
 #--nvtx --nvtx-include "gyroScatterEFF/" --set full \
 #--nvtx --nvtx-include "ion_push_op/" --set full \
 #--nvtx --nvtx-include "xgcm_scatterToMappedVerts/" --set full \
+#--nvtx --nvtx-include "gyroScatterEFF_cab/" --set full \
+#--nvtx --nvtx-include "savePtclPos/" --set full \
+
+# To profile multiple GPU kernels: use "kernel_A/*/kernel_B/*/kernel_C/"
