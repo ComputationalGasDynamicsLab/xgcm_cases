6a0b844716fe3e8d93cb71eccd9c17cdc6d39176
Chonglin Zhang <zhangchonglin@users.noreply.github.com>
2022-08-09 23:58:23 -0400

    Update xgcm_cyclone_Perlmutter_submission.sh

--- Cyclone_ITG_case/Profiling/Cyclone_590kmesh_Perlmutter/xgcm_cyclone_Perlmutter_submission.sh
+++ Cyclone_ITG_case/Profiling/Cyclone_590kmesh_Perlmutter/xgcm_cyclone_Perlmutter_submission.sh
@@ -11,11 +11,8 @@
 #SBATCH --job-name=Cyclone_590k
 
 module load PrgEnv-gnu
-module load systemlayer
-module load cuda/11.4.2
-module load cpe-cuda
+module load cudatoolkit/11.7
 module load craype-accel-nvidia80
-module load cmake/3.22.0
 export SLURM_CPU_BIND="cores"
 
 srun ./XGCm --kokkos-threads=1 590kmesh.osh 590kmesh_6.cpn \
