165bb44d3ada62986cc58dbeb48404eabbeba995
Chonglin Zhang <zhangchonglin@gmail.com>
2023-04-12 13:17:32 -0700

    use --kokkos-num-threads

--- D3D_ITG_case/kinetic_electron/400k_mesh/xgcm_D3D_400k_nphi=64.sh
+++ D3D_ITG_case/kinetic_electron/400k_mesh/xgcm_D3D_400k_nphi=64.sh
@@ -15,6 +15,6 @@ module load cudatoolkit/11.7
 module load craype-accel-nvidia80
 export SLURM_CPU_BIND="cores"
 
-srun ./XGCm --kokkos-threads=1 400k.osh 400k_4.cpn \
+srun ./XGCm --kokkos-num-threads=1 400k.osh 400k_4.cpn \
 1 1 bfs bfs 1 1 0 3 input_xgcm petsc petsc_xgcm.rc \
 -use_gpu_aware_mpi 0
