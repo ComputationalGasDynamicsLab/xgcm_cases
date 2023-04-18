041b9474d9e067c5463ec09c87fb4441ee968d83
Chonglin Zhang <zhangchonglin@gmail.com>
2023-04-18 14:37:40 -0700

    update kinetic electron case

---
+++ D3D_ITG_case/kinetic_electron/100k_mesh/xgcm_D3D_100k_nphi=32_nparts=4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -A m499
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -t 6:00:00
+#SBATCH -n 128
+#SBATCH --ntasks-per-node=4
+#SBATCH -c 32
+#SBATCH --gpus-per-task=1
+#SBATCH --gpu-bind=map_gpu:0,1,2,3
+#SBATCH --job-name=D3D_100k
+
+module load PrgEnv-gnu
+module load cudatoolkit/11.7
+module load craype-accel-nvidia80
+module unload darshan
+export SLURM_CPU_BIND="cores"
+
+srun ./XGCm --kokkos-threads=1 100k.osh 100k_4.cpn \
+4 1 bfs bfs 1 1 0 3 input_xgcm petsc petsc_xgcm.rc \
+-use_gpu_aware_mpi 0
