08d8bca2642a269a51182d8f3d188b21300c56e0
Chonglin Zhang <zhangchonglin@gmail.com>
2023-05-12 10:11:11 -0700

    update run script and add new run script with ddt

--- Cyclone_ITG_case/adiabatic_electron/Validation/590kmesh/xgcm_cyclone_Perlmutter_1rank_per_group_ddt.sh
+++ Cyclone_ITG_case/adiabatic_electron/Validation/590kmesh/xgcm_cyclone_Perlmutter_1rank_per_group_ddt.sh
@@ -4,16 +4,12 @@
 #SBATCH -q regular
 #SBATCH -t 0:30:00
 #SBATCH -N 2
-#SBATCH -n 8
-#SBATCH --ntasks-per-node=4
-#SBATCH -c 32
-#SBATCH --gpus-per-task=1
-#SBATCH --gpu-bind=single:1
 #SBATCH --job-name=Cyclone_590k
 
 module load PrgEnv-gnu
 module load cudatoolkit/11.7
 module load craype-accel-nvidia80
+module load arm-forge
 export SLURM_CPU_BIND="cores"
 export OMP_STACKSIZE=2G
 export OMP_PLACES=cores
@@ -21,7 +17,9 @@ export OMP_PROC_BIND=spread
 export OMP_NUM_THREADS=32
 ulimit -c unlimited
 
-srun --gpus-per-task=1 ./XGCm 590kmesh.osh 590kmesh_6.cpn \
+ddt --offline -o debug_output.html \
+srun -n 8 -c 32 --gpus-per-task=1 --gpu-bind=single:1 \
+./XGCm 590kmesh.osh 590kmesh_6.cpn \
 1 1 bfs bfs 1 0 0 3 input_xgcm petsc petsc_xgcm.rc \
 -use_gpu_aware_mpi 0
 
